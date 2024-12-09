1d11edb479d3fc1711b48d52a26350f65f48e39c
Isti-k <kiscserkes03@gmail.com>
2024-12-09 09:22:38 +0100

    f1 1,2

---
+++ src/main/java/hu/szamalk/Tabla.java
@@ -0,0 +1,11 @@
+package hu.szamalk;
+
+public class Tabla {
+    private char[][] t;
+    private char uresCella;
+
+    public Tabla(char[][] t, char uresCella) {
+        this.t = t;
+        this.uresCella = uresCella;
+    }
+}
